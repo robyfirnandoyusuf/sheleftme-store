5217fe3db2c08dc35edd720f27eb3e3c5b8de2c7
Roby Firnando Yusuf <0x3a3a3a@gmail.com>
2023-11-04 14:15:39 +0700

    fix: pre

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -25,30 +25,27 @@
 <section class="full_content_blog">
     <div class="container">
         <!-- <div class="details_section"> -->
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-8">
-                <div class="single_blog_item_5">
-                    <div class="blog_img">
-                        <img src="{{ $prod->image }}" alt="blog img">
-                    </div>
-                    <div class="blog_content">
-                        <div class="blog_content_bottom d-flex justify-content-between align-items-center">
-                            <a href="https://wa.me/6289674012988?text=*{{ $prod->title }}*, lagi available kah gan?" class="btn btn btn-primary" target="_blank">BELI <i class="ti-tag"></i></a>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8">
+                    <div class="single_blog_item_5">
+                        <div class="blog_img">
+                            <img src="{{ $prod->image }}" alt="blog img">
                         </div>
-                        <!-- <div class="blog_meta_list d-flex">
+                        <div class="blog_content">
+                            <div class="blog_content_bottom d-flex justify-content-between align-items-center">
+                                <a href="https://wa.me/6289674012988?text=*{{ $prod->title }}*, lagi available kah gan?" class="btn btn btn-primary" target="_blank">BELI <i class="ti-tag"></i></a>
+                            </div>
+                            <!-- <div class="blog_meta_list d-flex">
                                 <p>By Anderson in <a href="#">Photography</a> Posted 19 Aug 2021</p>
                             </div> -->
-                        <!-- <h2>Styles come and go. Design is a language, not a style.</h2> -->
-                        <div class="d-flex justify-content-center">
-                            <pre class="pre-style">
-                                {!! nl2br(e($prod->desc)) !!}
-                            </pre>
+                            <!-- <h2>Styles come and go. Design is a language, not a style.</h2> -->
+                            <div class="d-flex justify-content-center text-pre-wrap">
+                                <p>{!! nl2br(e($prod->desc)) !!}</p>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-        </div>
         <!-- </div> -->
     </div>
 </section>
